59d855716a85414e5164cffb4de6e11000dedda2
Khushi-152010 <80433045+Khushi-152010@users.noreply.github.com>
2021-04-29 20:50:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function change2() {
+    document.getElementById("li2").style.color = "red";
+}
+function change3() {
+    document.getElementById("li3").style.color = "yellow";
+}
+function change4() {
+    document.getElementById("li4").style.color = "lime";
+}
+function change5() {
+    document.getElementById("li5").style.color = "blue";
+}
